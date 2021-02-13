2dbe2b213b31572a0b5e9912202ef1258f11add0
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-02-13 13:00:44 -0500

    format webrtx signanling

--- lib/src/external/webrtc_signaling_crypto.dart
+++ lib/src/external/webrtc_signaling_crypto.dart
@@ -1,11 +1,10 @@
-  
 /* eslint-env browser */
 
-import * as encoding from 'lib0/encoding.js'
-import * as decoding from 'lib0/decoding.js'
-import * as promise from 'lib0/promise.js'
-import * as error from 'lib0/error.js'
-import * as string from 'lib0/string.js'
+import * as encoding from "lib0/encoding.js";
+import * as decoding from "lib0/decoding.js";
+import * as promise from "lib0/promise.js";
+import * as error from "lib0/error.js";
+import * as string from "lib0/string.js";
 
 /**
  * @param {string} secret
@@ -13,32 +12,28 @@ import * as string from 'lib0/string.js'
  * @return {PromiseLike<CryptoKey>}
  */
 export const deriveKey = (secret, roomName) => {
-  const secretBuffer = string.encodeUtf8(secret).buffer
-  const salt = string.encodeUtf8(roomName).buffer
-  return crypto.subtle.importKey(
-    'raw',
-    secretBuffer,
-    'PBKDF2',
-    false,
-    ['deriveKey']
-  ).then(keyMaterial =>
-    crypto.subtle.deriveKey(
-      {
-        name: 'PBKDF2',
-        salt,
-        iterations: 100000,
-        hash: 'SHA-256'
-      },
-      keyMaterial,
-      {
-        name: 'AES-GCM',
-        length: 256
-      },
-      true,
-      ['encrypt', 'decrypt']
-    )
-  )
-}
+  const secretBuffer = string.encodeUtf8(secret).buffer;
+  const salt = string.encodeUtf8(roomName).buffer;
+  return crypto.subtle
+    .importKey("raw", secretBuffer, "PBKDF2", false, ["deriveKey"])
+    .then((keyMaterial) =>
+      crypto.subtle.deriveKey(
+        {
+          name: "PBKDF2",
+          salt,
+          iterations: 100000,
+          hash: "SHA-256",
+        },
+        keyMaterial,
+        {
+          name: "AES-GCM",
+          length: 256,
+        },
+        true,
+        ["encrypt", "decrypt"]
+      )
+    );
+};
 
 /**
  * @param {Uint8Array} data data to be encrypted
@@ -47,24 +42,26 @@ export const deriveKey = (secret, roomName) => {
  */
 export const encrypt = (data, key) => {
   if (!key) {
-    return /** @type {PromiseLike<Uint8Array>} */ (promise.resolve(data))
+    return /** @type {PromiseLike<Uint8Array>} */ (promise.resolve(data));
   }
-  const iv = crypto.getRandomValues(new Uint8Array(12))
-  return crypto.subtle.encrypt(
-    {
-      name: 'AES-GCM',
-      iv
-    },
-    key,
-    data
-  ).then(cipher => {
-    const encryptedDataEncoder = encoding.createEncoder()
-    encoding.writeVarString(encryptedDataEncoder, 'AES-GCM')
-    encoding.writeVarUint8Array(encryptedDataEncoder, iv)
-    encoding.writeVarUint8Array(encryptedDataEncoder, new Uint8Array(cipher))
-    return encoding.toUint8Array(encryptedDataEncoder)
-  })
-}
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  return crypto.subtle
+    .encrypt(
+      {
+        name: "AES-GCM",
+        iv,
+      },
+      key,
+      data
+    )
+    .then((cipher) => {
+      const encryptedDataEncoder = encoding.createEncoder();
+      encoding.writeVarString(encryptedDataEncoder, "AES-GCM");
+      encoding.writeVarUint8Array(encryptedDataEncoder, iv);
+      encoding.writeVarUint8Array(encryptedDataEncoder, new Uint8Array(cipher));
+      return encoding.toUint8Array(encryptedDataEncoder);
+    });
+};
 
 /**
  * @param {Object} data data to be encrypted
@@ -72,10 +69,10 @@ export const encrypt = (data, key) => {
  * @return {PromiseLike<Uint8Array>} encrypted data, if key is provided
  */
 export const encryptJson = (data, key) => {
-  const dataEncoder = encoding.createEncoder()
-  encoding.writeAny(dataEncoder, data)
-  return encrypt(encoding.toUint8Array(dataEncoder), key)
-}
+  const dataEncoder = encoding.createEncoder();
+  encoding.writeAny(dataEncoder, data);
+  return encrypt(encoding.toUint8Array(dataEncoder), key);
+};
 
 /**
  * @param {Uint8Array} data
@@ -84,24 +81,26 @@ export const encryptJson = (data, key) => {
  */
 export const decrypt = (data, key) => {
   if (!key) {
-    return /** @type {PromiseLike<Uint8Array>} */ (promise.resolve(data))
+    return /** @type {PromiseLike<Uint8Array>} */ (promise.resolve(data));
   }
-  const dataDecoder = decoding.createDecoder(data)
-  const algorithm = decoding.readVarString(dataDecoder)
-  if (algorithm !== 'AES-GCM') {
-    promise.reject(error.create('Unknown encryption algorithm'))
+  const dataDecoder = decoding.createDecoder(data);
+  const algorithm = decoding.readVarString(dataDecoder);
+  if (algorithm !== "AES-GCM") {
+    promise.reject(error.create("Unknown encryption algorithm"));
   }
-  const iv = decoding.readVarUint8Array(dataDecoder)
-  const cipher = decoding.readVarUint8Array(dataDecoder)
-  return crypto.subtle.decrypt(
-    {
-      name: 'AES-GCM',
-      iv
-    },
-    key,
-    cipher
-  ).then(data => new Uint8Array(data))
-}
+  const iv = decoding.readVarUint8Array(dataDecoder);
+  const cipher = decoding.readVarUint8Array(dataDecoder);
+  return crypto.subtle
+    .decrypt(
+      {
+        name: "AES-GCM",
+        iv,
+      },
+      key,
+      cipher
+    )
+    .then((data) => new Uint8Array(data));
+};
 
 /**
  * @param {Uint8Array} data
@@ -109,6 +108,6 @@ export const decrypt = (data, key) => {
  * @return {PromiseLike<Object>} decrypted object
  */
 export const decryptJson = (data, key) =>
-  decrypt(data, key).then(decryptedValue =>
+  decrypt(data, key).then((decryptedValue) =>
     decoding.readAny(decoding.createDecoder(new Uint8Array(decryptedValue)))
-  )
+  );
