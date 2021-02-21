6825ea17520ac27b54018eba6728fd447aad290a
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-02-21 17:28:56 -0500

    websocket from lib0

--- lib/src/external/websocket/websocket.dart
+++ lib/src/external/websocket/websocket.dart
@@ -1,5 +1,12 @@
 /* eslint-env browser */
 
+import 'dart:math' as math;
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:y_crdt/src/utils/observable.dart';
+
 /**
  * Tiny websocket connection handler.
  *
@@ -8,146 +15,180 @@
  * @module websocket
  */
 
-import { Observable } from './observable.js'
-import * as time from './time.js'
-import * as math from './math.js'
+// import { Observable } from "./observable.js";
+// import * as time from "./time.js";
+// import * as math from "./math.js";
 
-const reconnectTimeoutBase = 1200
-const maxReconnectTimeout = 2500
+const reconnectTimeoutBase = 1200;
+const maxReconnectTimeout = 2500;
 // @todo - this should depend on awareness.outdatedTime
-const messageReconnectTimeout = 30000
+const messageReconnectTimeout = 30000;
 
 /**
  * @param {WebsocketClient} wsclient
  */
-const setupWS = (wsclient) => {
-  if (wsclient.shouldConnect && wsclient.ws === null) {
-    const websocket = new WebSocket(wsclient.url)
-    const binaryType = wsclient.binaryType
+void setupWS(WebsocketClient wsclient) {
+  if (wsclient.shouldConnect && wsclient.ws == null) {
+    final websocket = WebSocketChannel.connect(Uri.parse(wsclient.url));
+    // const binaryType = wsclient.binaryType;
     /**
      * @type {any}
      */
-    let pingTimeout = null
-    if (binaryType) {
-      websocket.binaryType = binaryType
-    }
-    wsclient.ws = websocket
-    wsclient.connecting = true
-    wsclient.connected = false
-    websocket.onmessage = event => {
-      wsclient.lastMessageReceived = time.getUnixTime()
-      const data = event.data
-      const message = typeof data === 'string' ? JSON.parse(data) : data
-      if (message && message.type === 'pong') {
-        clearTimeout(pingTimeout)
-        pingTimeout = setTimeout(sendPing, messageReconnectTimeout / 2)
+    Timer? pingTimeout;
+    // if (binaryType) {
+    //   websocket.binaryType = binaryType;
+    // }
+    final sendPing = () {
+      if (wsclient.ws == websocket) {
+        wsclient.send({
+          "type": "ping",
+        });
       }
-      wsclient.emit('message', [message, wsclient])
-    }
+    };
+    wsclient.ws = websocket;
+    wsclient.connecting = true;
+    wsclient.connected = false;
+
     /**
      * @param {any} error
      */
-    const onclose = error => {
-      if (wsclient.ws !== null) {
-        wsclient.ws = null
-        wsclient.connecting = false
+    final onclose = (error) {
+      if (wsclient.ws != null) {
+        wsclient.ws = null;
+        wsclient.connecting = false;
         if (wsclient.connected) {
-          wsclient.connected = false
-          wsclient.emit('disconnect', [{ type: 'disconnect', error }, wsclient])
+          wsclient.connected = false;
+          wsclient.emit("disconnect", [
+            {"type": "disconnect", "error": error},
+            wsclient,
+          ]);
         } else {
-          wsclient.unsuccessfulReconnects++
+          wsclient.unsuccessfulReconnects++;
         }
         // Start with no reconnect timeout and increase timeout by
         // log10(wsUnsuccessfulReconnects).
         // The idea is to increase reconnect timeout slowly and have no reconnect
         // timeout at the beginning (log(1) = 0)
-        setTimeout(setupWS, math.min(math.log10(wsclient.unsuccessfulReconnects + 1) * reconnectTimeoutBase, maxReconnectTimeout), wsclient)
+        Future.delayed(
+          Duration(
+              milliseconds: math
+                  .min(
+                      math.log(wsclient.unsuccessfulReconnects + 1) *
+                          reconnectTimeoutBase,
+                      maxReconnectTimeout)
+                  .toInt()),
+          () => setupWS(wsclient),
+        );
       }
-      clearTimeout(pingTimeout)
-    }
-    const sendPing = () => {
-      if (wsclient.ws === websocket) {
-        wsclient.send({
-          type: 'ping'
-        })
-      }
-    }
-    websocket.onclose = () => onclose(null)
-    websocket.onerror = error => onclose(error)
-    websocket.onopen = () => {
-      wsclient.lastMessageReceived = time.getUnixTime()
-      wsclient.connecting = false
-      wsclient.connected = true
-      wsclient.unsuccessfulReconnects = 0
-      wsclient.emit('connect', [{ type: 'connect' }, wsclient])
+      pingTimeout?.cancel();
+    };
+
+    websocket.stream.listen(
+      (data) {
+        wsclient.lastMessageReceived = DateTime.now().millisecondsSinceEpoch;
+        final message = data is String ? jsonDecode(data) : data;
+        if (message is Map<String, Object?> && message["type"] == "pong") {
+          pingTimeout?.cancel();
+          pingTimeout = Timer(
+            Duration(milliseconds: messageReconnectTimeout ~/ 2),
+            sendPing,
+          );
+        }
+        wsclient.emit("message", [message, wsclient]);
+      },
+      onDone: () => onclose(null),
+      onError: (error) => onclose(error),
+    );
+
+    websocket.onOpen = () {
+      wsclient.lastMessageReceived = DateTime.now().millisecondsSinceEpoch;
+      wsclient.connecting = false;
+      wsclient.connected = true;
+      wsclient.unsuccessfulReconnects = 0;
+      wsclient.emit("connect", [
+        {"type": "connect"},
+        wsclient
+      ]);
       // set ping
-      pingTimeout = setTimeout(sendPing, messageReconnectTimeout / 2)
-    }
+      pingTimeout = Timer(
+        Duration(milliseconds: messageReconnectTimeout ~/ 2),
+        sendPing,
+      );
+    };
   }
 }
 
 /**
  * @extends Observable<string>
  */
-export class WebsocketClient extends Observable {
+class WebsocketClient extends Observable<String> {
+  final String url;
+  /**
+   * @type {WebSocket?}
+   */
+  WebSocketChannel? ws;
+  // bool binaryType = binaryType || null;
+  bool connected = false;
+  bool connecting = false;
+  int unsuccessfulReconnects = 0;
+  int lastMessageReceived = 0;
+  /**
+   * Whether to connect to other peers or not
+   * @type {boolean}
+   */
+  bool shouldConnect = true;
+
+  late final Timer _checkInterval;
   /**
    * @param {string} url
    * @param {object} [opts]
    * @param {'arraybuffer' | 'blob' | null} [opts.binaryType] Set `ws.binaryType`
    */
-  constructor (url, { binaryType } = {}) {
-    super()
-    this.url = url
-    /**
-     * @type {WebSocket?}
-     */
-    this.ws = null
-    this.binaryType = binaryType || null
-    this.connected = false
-    this.connecting = false
-    this.unsuccessfulReconnects = 0
-    this.lastMessageReceived = 0
-    /**
-     * Whether to connect to other peers or not
-     * @type {boolean}
-     */
-    this.shouldConnect = true
-    this._checkInterval = setInterval(() => {
-      if (this.connected && messageReconnectTimeout < time.getUnixTime() - this.lastMessageReceived) {
-        // no message received in a long time - not even your own awareness
-        // updates (which are updated every 15 seconds)
-        /** @type {WebSocket} */ (this.ws).close()
-      }
-    }, messageReconnectTimeout / 2)
-    setupWS(this)
+  WebsocketClient(this.url) {
+    this._checkInterval = Timer.periodic(
+      Duration(milliseconds: messageReconnectTimeout ~/ 2),
+      (_) {
+        if (this.connected &&
+            messageReconnectTimeout <
+                DateTime.now().millisecondsSinceEpoch -
+                    this.lastMessageReceived) {
+          // no message received in a long time - not even your own awareness
+          // updates (which are updated every 15 seconds)
+          /** @type {WebSocket} */ (this.ws!).sink.close();
+        }
+      },
+    );
+
+    setupWS(this);
   }
 
   /**
    * @param {any} message
    */
-  send (message) {
-    if (this.ws) {
-      this.ws.send(JSON.stringify(message))
+  void send(message) {
+    if (this.ws != null) {
+      this.ws!.sink.add(jsonEncode(message));
     }
   }
 
-  destroy () {
-    clearInterval(this._checkInterval)
-    this.disconnect()
-    super.destroy()
+  @override
+  void destroy() {
+    this._checkInterval.cancel();
+    this.disconnect();
+    super.destroy();
   }
 
-  disconnect () {
-    this.shouldConnect = false
-    if (this.ws !== null) {
-      this.ws.close()
+  void disconnect() {
+    this.shouldConnect = false;
+    if (this.ws != null) {
+      this.ws!.sink.close();
     }
   }
 
-  connect () {
-    this.shouldConnect = true
-    if (!this.connected && this.ws === null) {
-      setupWS(this)
+  void connect() {
+    this.shouldConnect = true;
+    if (!this.connected && this.ws == null) {
+      setupWS(this);
     }
   }
 }
